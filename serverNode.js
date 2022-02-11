e65c7d845ebb43b6d90117609e98fef6bd79dd25
LuxemTheFez <luxem192@gmail.com>
2022-02-11 09:56:38 +0100

    Init

---
+++ serverNode.js
@@ -0,0 +1,71 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var soap = require('soap');
+const request = require('request');
+
+var url = 'https://soapservice-info802.herokuapp.com/?wsdl';
+var args = {};
+
+app.get('/', function(req, res){
+    res.sendFile('C:/Users/Luxem/Documents/Cours/M1/INFO802/service/clientHtml.html');
+   });
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+   });
+   
+io.on('connection', function(socket){
+
+    socket.on('requestCarNameArray',function(){
+        soap.createClient(url, function(err, client) {
+            client.get_car(args, function(err, result) {
+                let evalData = JSON.parse(result.get_carResult);
+                evalData.forEach(car => res.push(car['model']));
+                io.emit('sendCarNameArray', res)
+            });
+        });
+    })
+
+    socket.on('requestCarArray',function(){
+        soap.createClient(url, function(err, client) {
+            client.get_car(args, function(err, result) {
+                let evalData = JSON.parse(result.get_carResult);
+                io.emit('sendCarArray', evalData)
+            });
+        });
+    })
+    
+    socket.on('calculateRideTime',function(data, rideLength, averageSpeed){
+        console.log(data)
+        if (rideLength!=0 && averageSpeed!=0){
+            var urlApi = `http://localhost:5000/Car?rideLength=${rideLength}&fastChargeTime=${data['fastChargeTime']}&chargeTime=${data['chargeTime']}&batterySize=${data['range']}&averageSpeed=${averageSpeed}`;
+            console.log(urlApi)
+            request(urlApi, (error, response, body)=>{
+        
+                // Printing the error if occurred
+                if(error) console.log(error)
+                
+                // Printing status code
+                console.log(response.statusCode);
+                
+                // Printing body
+                console.log(secondsToHms(body));
+                io.emit('getRideTime', secondsToHms(body))
+
+            }); 
+    }
+    })
+
+    function secondsToHms(d) {
+        d = Number(d);
+        var h = Math.floor(d / 3600);
+        var m = Math.floor(d % 3600 / 60);
+        var s = Math.floor(d % 3600 % 60);
+    
+        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        return hDisplay + mDisplay + sDisplay; 
+    }
+
+});
